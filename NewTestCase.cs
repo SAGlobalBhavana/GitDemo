940b0af47700d98bf09c4a08d4339d512c0fd18b
Bhavana <BhavanaaR@saglobal.com>
2025-04-16 16:19:15 +0530

    Create NewTestCase.cs
    Created the Testcase class

---
+++ NewTestCase.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace AmazonSearchTest
+{
+    [TestClass]
+    public class AmazonSearchTests
+    {
+    }
+}
