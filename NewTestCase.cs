9ff6121948caa4d6fe5898f5d23811f7c9683a83
Bhavana <BhavanaaR@saglobal.com>
2025-04-21 16:08:56 +0530

    Feature 3 Update NewTestCase.cs

--- NewTestCase.cs
+++ NewTestCase.cs
@@ -4,6 +4,9 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 // - Added by BHavanaa 
+// Feature 3 added 
+// to add the cleanup code
+
 // Feature 2 -    // Find and click the search button
  // Find and click the search button - Commit
             IWebElement searchButton = driver.FindElement(By.Id("nav-search-submit-button"));
