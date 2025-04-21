a2125430f956f8167228a2deaa0f9542c0c8f788
Bhavanaa J <BhavanaaR@saglobal.com>
2025-04-21 10:18:36 +0530

    Created Testcase class

---
+++ TestCase.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace AmazonSearchTest
+{
+    [TestClass]
+    public class AmazonSearchTests
+    {
+        //Testing
+    }
+}
+
